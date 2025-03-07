d1e08cf72a0c2503018f44ab1c13ab0cf3635a12
JungSoonJae <34623431+jjh75607@users.noreply.github.com>
2025-03-07 16:01:40 +0900

    [#6] Fix: Update ID annotation import in JWTRefreshToken and OauthToken classes (#7)

--- src/main/java/soon/capstone/global/redis/domain/jwt/entity/JWTRefreshToken.java
+++ src/main/java/soon/capstone/global/redis/domain/jwt/entity/JWTRefreshToken.java
@@ -1,10 +1,10 @@
 package soon.capstone.global.redis.domain.jwt.entity;
 
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
