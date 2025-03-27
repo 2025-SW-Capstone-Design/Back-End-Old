f2bd9d0247806a3267fad09b7c50278f4fbe094f
zzangjyj0818 <jyj010818@naver.com>
2025-03-27 21:10:20 +0900

    [#19] Refactor: Add exception handling related to Github API (GraphQL) communication and remove unnecessary code.

--- src/main/java/soon/capstone/infrastructure/github/service/GithubOrganizationReadService.java
+++ src/main/java/soon/capstone/infrastructure/github/service/GithubOrganizationReadService.java
@@ -2,6 +2,7 @@ package soon.capstone.infrastructure.github.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import soon.capstone.global.exception.github.GithubHttpClientException;
 import soon.capstone.infrastructure.github.service.constant.GithubQuery;
 import soon.capstone.infrastructure.graphQL.GraphQLClientConfig;
 
@@ -12,14 +13,17 @@ public class GithubOrganizationReadService {
     private final GraphQLClientConfig graphQLClientConfig;
 
     public String getOrganizationId(String organizationName, String oauthToken) {
-
         String formattedQuery = String.format(GithubQuery.GET_ORGANIZATION.getQuery(), organizationName);
 
-        return graphQLClientConfig.queryClient(oauthToken)
-                .document(formattedQuery)
-                .retrieve("organization.id")
-                .toEntity(String.class)
-                .block();
+        try {
+            return graphQLClientConfig.queryClient(oauthToken)
+                    .document(formattedQuery)
+                    .retrieve("organization.id")
+                    .toEntity(String.class)
+                    .block();
+        } catch (Exception e) {
+            throw new GithubHttpClientException();
+        }
     }
 
 }
