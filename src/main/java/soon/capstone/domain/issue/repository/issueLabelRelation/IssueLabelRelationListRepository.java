f930868227cffdf0ba2a81f1a21293c102dae804
zzangjyj0818 <jyj010818@naver.com>
2025-04-11 17:48:09 +0900

    [#35] Refactor: Remove unused methods.

--- src/main/java/soon/capstone/domain/issue/repository/issueLabelRelation/IssueLabelRelationListRepository.java
+++ src/main/java/soon/capstone/domain/issue/repository/issueLabelRelation/IssueLabelRelationListRepository.java
@@ -1,9 +1,5 @@
 package soon.capstone.domain.issue.repository.issueLabelRelation;
 
-import soon.capstone.domain.issue.service.dto.response.IssueLabelDetailResponse;
-
-import java.util.List;
-
 public interface IssueLabelRelationListRepository {
-    List<IssueLabelDetailResponse> getIssueLabelsByIssueId(Long issueId);
+
 }
--- src/main/java/soon/capstone/domain/issue/repository/issueLabelRelation/IssueLabelRelationListRepositoryImpl.java
+++ src/main/java/soon/capstone/domain/issue/repository/issueLabelRelation/IssueLabelRelationListRepositoryImpl.java
@@ -3,32 +3,11 @@ package soon.capstone.domain.issue.repository.issueLabelRelation;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import soon.capstone.domain.issue.service.dto.response.IssueLabelDetailResponse;
-
-import java.util.List;
-
-import static com.querydsl.core.types.Projections.fields;
-import static soon.capstone.domain.issue.entity.QIssueLabel.issueLabel;
-import static soon.capstone.domain.issue.entity.QIssueLabelRelation.issueLabelRelation;
 
 @RequiredArgsConstructor
 @Repository
 public class IssueLabelRelationListRepositoryImpl implements IssueLabelRelationListRepository {
 
     private final JPAQueryFactory queryFactory;
-
-    @Override
-    public List<IssueLabelDetailResponse> getIssueLabelsByIssueId(Long issueId) {
-        return queryFactory.select(fields(IssueLabelDetailResponse.class,
-                        issueLabel.id,
-                        issueLabel.title.as("name"),
-                        issueLabel.color,
-                        issueLabel.description
-                ))
-                .from(issueLabelRelation)
-                .join(issueLabelRelation.issueLabel, issueLabel)
-                .where(issueLabelRelation.issue.id.eq(issueId))
-                .fetch();
-    }
 
 }
