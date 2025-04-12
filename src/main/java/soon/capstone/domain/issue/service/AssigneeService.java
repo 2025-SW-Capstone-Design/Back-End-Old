af8c64c9a532d2fe7259e1e1714e1d8a625d3b91
jjh75607 <dkrak5738@naver.com>
2025-04-12 17:40:06 +0900

    [#41] Feat: Add appendIssueWithAssignee method to AssigneeService for appending assignees to issues

--- src/main/java/soon/capstone/domain/issue/service/AssigneeService.java
+++ src/main/java/soon/capstone/domain/issue/service/AssigneeService.java
@@ -3,6 +3,7 @@ package soon.capstone.domain.issue.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import soon.capstone.infrastructure.github.service.dto.GithubAssigneesAppendServiceRequest;
 import soon.capstone.infrastructure.github.service.dto.GithubAssigneesServiceRequest;
 import soon.capstone.infrastructure.github.service.issue.GithubAssigneesService;
 
@@ -26,6 +27,23 @@ public class AssigneeService {
             .memberId(memberId)
             .build();
         return githubAssigneesService.isAssignee(request);
+    }
+
+    public void appendIssueWithAssignee(
+        Long memberId,
+        Long issueNumber,
+        String organizationName,
+        String repositoryName,
+        String assignees
+    ) {
+        GithubAssigneesAppendServiceRequest request = GithubAssigneesAppendServiceRequest.builder()
+            .memberId(memberId)
+            .issueNumber(issueNumber)
+            .organizationName(organizationName)
+            .repositoryName(repositoryName)
+            .assignee(assignees)
+            .build();
+        githubAssigneesService.appendIssueWithAssignee(request);
     }
 
 }
