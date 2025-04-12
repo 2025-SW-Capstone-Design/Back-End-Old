7820e17d73f5f1b0d751470090da8d0d62f1d166
jjh75607 <dkrak5738@naver.com>
2025-04-12 22:26:17 +0900

    [#57] Feat: Add Update functionality for Github issue

--- src/main/java/soon/capstone/infrastructure/github/service/issue/GithubIssueService.java
+++ src/main/java/soon/capstone/infrastructure/github/service/issue/GithubIssueService.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import soon.capstone.global.exception.github.GithubHttpClientException;
 import soon.capstone.infrastructure.github.service.dto.GithubIssueCreateServiceRequest;
+import soon.capstone.infrastructure.github.service.dto.GithubIssueUpdateServiceRequest;
 import soon.capstone.infrastructure.github.service.dto.response.GithubIssueCreateResponse;
 import soon.capstone.infrastructure.redis.oauth2.repository.OAuthTokenRepository;
 import soon.capstone.infrastructure.restclient.config.RestClientConfig;
@@ -42,6 +43,38 @@ public class GithubIssueService {
             throw new GithubHttpClientException();
         } catch (Exception e) {
             log.error("issue 추가 중 에러 발생", e);
+            throw new GithubHttpClientException();
+        }
+    }
+
+    /**
+     * GitHub Issue 수정
+     *
+     * @param request labels가 빈 List일 경우 이슈의 label이 모두 삭제됨
+     *                assignees가 null일 경우 이슈의 assignee가 모두 삭제됨
+     *                둘다 모두 이전의 내용을 포함하지 않을경우 유지되지 않음
+     */
+    public void updateGithubIssue(GithubIssueUpdateServiceRequest request) {
+        try {
+            String token = oAuthTokenRepository.findByMemberId(request.memberId()).getToken();
+            RestClient restClient = restClientConfig.githubRestClient(token);
+
+            String uri = (ISSUE_URL + "/{issueNumber}")
+                .replace("{organizationName}", request.organizationName())
+                .replace("{repositoryName}", request.repositoryName())
+                .replace("{issueNumber}", String.valueOf(request.issueNumber()));
+
+            restClient.patch()
+                .uri(uri)
+                .body(request.toGithubRequest())
+                .retrieve()
+                .body(Void.class);
+
+        } catch (HttpClientErrorException e) {
+            log.error("GitHub API 호출 중 오류 발생: {}", e.getMessage(), e);
+            throw new GithubHttpClientException();
+        } catch (Exception e) {
+            log.error("issue 수정 중 에러 발생", e);
             throw new GithubHttpClientException();
         }
     }
