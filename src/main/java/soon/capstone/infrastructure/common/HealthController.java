d8d0d4bc49af3e361cca87e6cffd9f395e0a51ea
zzangjyj0818 <jyj010818@naver.com>
2025-04-19 18:49:24 +0900

    [#70] Chore: Edit a endpoint about HealthController

--- src/main/java/soon/capstone/infrastructure/common/HealthController.java
+++ src/main/java/soon/capstone/infrastructure/common/HealthController.java
@@ -2,9 +2,11 @@ package soon.capstone.infrastructure.common;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/health")
+@RequestMapping("/api/v1/health")
+@RestController
 public class HealthController {
 
     @GetMapping
