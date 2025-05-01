37c1bf98ca2284ba317777e36e6b64df95f57aff
jjh75607 <dkrak5738@naver.com>
2025-05-01 16:40:04 +0900

    [#84] Refactor: Add team association to ChatRoom entity

--- src/main/java/soon/capstone/domain/chatroom/entity/ChatRoom.java
+++ src/main/java/soon/capstone/domain/chatroom/entity/ChatRoom.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import soon.capstone.domain.team.entity.Team;
 import soon.capstone.global.common.BaseTimeEntity;
 import soon.capstone.global.exception.common.InvalidRequest;
 
@@ -30,18 +31,24 @@ public class ChatRoom extends BaseTimeEntity {
     @Column(name = "reserved_at", nullable = true)
     private LocalDateTime reservedAt;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
     @Builder
-    private ChatRoom(String title, boolean active, LocalDateTime reservedAt) {
+    private ChatRoom(String title, boolean active, LocalDateTime reservedAt, Team team) {
         this.title = title;
         this.active = active;
         this.reservedAt = reservedAt;
+        this.team = team;
     }
 
-    public static ChatRoom create(String title, LocalDateTime reservedAt) {
+    public static ChatRoom create(String title, LocalDateTime reservedAt, Team team) {
         return ChatRoom.builder()
             .title(title)
             .active(true)
             .reservedAt(reservedAt)
+            .team(team)
             .build();
     }
 
