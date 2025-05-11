f1ac4771bc4aa40e4d1a94ab93f51068300a1440
jjh75607 <dkrak5738@naver.com>
2025-05-11 21:44:13 +0900

    [#105] Feat: Implement OpenAI RestClient configuration

--- src/main/java/soon/capstone/infrastructure/restclient/config/RestClientConfig.java
+++ src/main/java/soon/capstone/infrastructure/restclient/config/RestClientConfig.java
@@ -1,15 +1,21 @@
 package soon.capstone.infrastructure.restclient.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static soon.capstone.global.security.jwt.common.TokenType.AUTHORIZATION_HEADER;
 import static soon.capstone.global.security.jwt.common.TokenType.BEARER_PREFIX;
 
 @Component
 public class RestClientConfig {
 
+    @Value("${openai.api.key}")
+    private String openAiKey;
+
     private static final String GITHUB_BASE_URL = "https://api.github.com";
+    private static final String OPEN_AI_BASE_URL = "https://api.openai.com/v1/chat/completions";
 
     public RestClient githubRestClient(String oauth2Token) {
         return RestClient.builder()
@@ -20,7 +26,11 @@ public class RestClientConfig {
     }
 
     public RestClient openAiRestClient() {
-        throw new UnsupportedOperationException("openAiRestClient is not implemented");
+        return RestClient.builder()
+            .baseUrl(OPEN_AI_BASE_URL)
+            .defaultHeader(AUTHORIZATION_HEADER.getValue(), BEARER_PREFIX.getValue() + openAiKey)
+            .defaultHeader("Content-Type", APPLICATION_JSON_VALUE)
+            .build();
     }
 
 }
