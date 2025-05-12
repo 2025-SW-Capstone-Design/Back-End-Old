76441a62304753fed1cdad56c332ecff140e7a9b
jjh75607 <dkrak5738@naver.com>
2025-05-13 00:48:19 +0900

    [#122] Test: Functional test commit

--- src/main/java/soon/capstone/infrastructure/openvidu/controller/OpenViduController.java
+++ src/main/java/soon/capstone/infrastructure/openvidu/controller/OpenViduController.java
@@ -1,7 +1,9 @@
 package soon.capstone.infrastructure.openvidu.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import soon.capstone.global.anootation.AuthMemberId;
@@ -10,6 +12,7 @@ import soon.capstone.infrastructure.openvidu.controller.dto.request.OpenViduWebh
 import soon.capstone.infrastructure.openvidu.service.OpenViduApiService;
 import soon.capstone.infrastructure.openvidu.service.dto.response.OpenViduGenerateTokenResponse;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/open-vidu")
 @RestController
@@ -28,11 +31,19 @@ public class OpenViduController {
 
     @PostMapping(value = "/webhook", consumes = "application/webhook+json")
     public ResponseEntity<Long> receiveWebhook(
-        @Valid @RequestBody OpenViduWebhookEventRequest request,
+        HttpServletRequest servletRequest,
+        @RequestBody String body,
+//        @Valid @RequestBody OpenViduWebhookEventRequest request,
         @RequestHeader(value = "Authorization") String openViduToken
     ) {
-        Long chatRoomId = openViduApiService.handleWebhookEvent(request.toServiceRequest(2L, openViduToken));
-        return ResponseEntity.ok(chatRoomId);
+        String requestUrl = servletRequest.getRequestURL().toString();
+        String method = servletRequest.getMethod();
+        String clientIp = servletRequest.getRemoteAddr();
+        log.info("Received Request - URL: {}, Method: {}, Client IP: {}", requestUrl, method, clientIp);
+        log.info("Received Body: {}", body);
+
+//        Long chatRoomId = openViduApiService.handleWebhookEvent(request.toServiceRequest(2L, openViduToken));
+        return ResponseEntity.ok(1L);
     }
 
 }
