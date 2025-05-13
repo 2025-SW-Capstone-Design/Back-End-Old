8117727a7741127c6f7a77a561411405bc70e737
jjh75607 <dkrak5738@naver.com>
2025-05-14 00:58:39 +0900

    [#122] Test: Functional test commit

--- src/main/java/soon/capstone/infrastructure/openvidu/controller/OpenViduController.java
+++ src/main/java/soon/capstone/infrastructure/openvidu/controller/OpenViduController.java
@@ -1,15 +1,21 @@
 package soon.capstone.infrastructure.openvidu.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 import soon.capstone.global.anootation.AuthMemberId;
 import soon.capstone.infrastructure.openvidu.controller.dto.request.OpenViduGenerateTokenRequest;
 import soon.capstone.infrastructure.openvidu.controller.dto.request.OpenViduWebhookEventRequest;
 import soon.capstone.infrastructure.openvidu.service.OpenViduApiService;
 import soon.capstone.infrastructure.openvidu.service.dto.response.OpenViduGenerateTokenResponse;
 
+import java.io.IOException;
+
+@Slf4j
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/open-vidu")
@@ -29,9 +35,15 @@ public class OpenViduController {
 
     @PostMapping(value = "/webhook", consumes = "application/webhook+json")
     public ResponseEntity<Long> receiveWebhook(
+        HttpServletRequest servletRequest,
         @Valid @RequestBody OpenViduWebhookEventRequest request,
         @RequestHeader(value = "Authorization") String openViduToken
-    ) {
+    ) throws IOException {
+        ContentCachingRequestWrapper wrapper = new ContentCachingRequestWrapper(servletRequest);
+        wrapper.getInputStream().readAllBytes();
+        String requestBody = new String(wrapper.getContentAsByteArray());
+        log.info("requestBody: {}", requestBody);
+
         Long chatRoomId = openViduApiService.handleWebhookEvent(request.toServiceRequest(openViduToken));
         return ResponseEntity.ok(chatRoomId);
     }
