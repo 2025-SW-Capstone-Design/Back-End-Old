73122f5de78476561abe6b0c8000bae15d01162f
jjh75607 <dkrak5738@naver.com>
2025-05-15 15:09:28 +0900

    [#133] Refactor: Update content column definition to TEXT in multiple entities

--- src/main/java/soon/capstone/domain/portfolio/entity/Portfolio.java
+++ src/main/java/soon/capstone/domain/portfolio/entity/Portfolio.java
@@ -22,8 +22,7 @@ public class Portfolio extends BaseTimeEntity {
     @Column(nullable = false)
     private String title;
 
-    @Lob
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
